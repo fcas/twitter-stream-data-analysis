14780d154718a73b70f4753d9c2554b69705f20d
System Administrator <root@Felipes-MacBook-Air.local>
2016-04-11 22:24:52 -0300

    project commit

---
+++ abbreviations.py
@@ -0,0 +1,18 @@
+words = \
+    {
+        'n': 'não',
+        'nao': 'não',
+        'naum': 'não',
+        'nop': 'não',
+        'sqn': 'não',
+        'pq': 'porque',
+        'p q': 'por que',
+        'msm': 'mesmo',
+        'tá': 'está',
+        'kkk': 'engraçado',
+        'rs': 'engraçado',
+        'rsrs': 'engraçado',
+        'rsrsrs': 'engraçado',
+        'hahaha': 'engraçado',
+        'q': 'que'
+    }
--- stream_processor.py
+++ stream_processor.py
@@ -1,24 +1,26 @@
 import sys
 import json
-import model
 import config
 import signal
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+from sentiment_analysis import save
+from sentiment_analysis import tweet_processor
+
+
+def sigint_handler(signal, frame):
+    save()
 
 
 class StdOutListener(StreamListener):
     def on_data(self, data):
         tweet = json.loads(data, encoding='utf-8')
-        model.tweet_processor(tweet)
+        tweet_processor(tweet)
 
     def on_error(self, status):
         print(status)
 
-
-def sigint_handler(signal, frame):
-    model.save_model()
 
 if __name__ == '__main__':
     try:
@@ -26,6 +28,7 @@ if __name__ == '__main__':
         auth = OAuthHandler(config.keys['consumer_key'], config.keys['consumer_secret'])
         auth.set_access_token(config.keys['access_token'], config.keys['access_token_secret'])
         stream = Stream(auth, listener)
+
         signal.signal(signal.SIGINT, sigint_handler)
         # stream.filter(track=['Curitiba_PMC', 'belemfala', 'prefsp', 'Prefeitura_Rio', 'prefeiturabh'])
         stream.filter(track=['pecesiqueira', 'naosalvo', 'rafinhabastos', 'jovemnerd', 'mkarolqueiroz', 'felipenetoreal', 'belpesce', 'dilma', 'cunha'])
